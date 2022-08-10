f12d030936a2c678680759860604429c510316a5
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-08-10 23:32:09 +0900

    [Chore] 임시 토큰 기한 변경

--- src/main/java/com/firefighter/aenitto/auth/service/TokenService.java
+++ src/main/java/com/firefighter/aenitto/auth/service/TokenService.java
@@ -28,7 +28,7 @@ public class TokenService {
     }
 
     public Token generateToken(String uid, String role) {
-        long tokenPeriod = 1000L * 60L * 90L;
+        long tokenPeriod = 1000L * 60L * 60L * 24L * 30L * 3L;
         long refreshPeriod = 1000L * 60L * 60L * 24L * 30L * 3L;
         System.out.println("generating token");
 
---
+++ src/test/java/com/firefighter/aenitto/rooms/integration/RoomIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.firefighter.aenitto.rooms.integration;
+
+import com.firefighter.aenitto.support.IntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoomIntegrationTest extends IntegrationTest {
+
+}
