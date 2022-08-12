b7b389aaecded4c16d0c227312dc3552a849ba06
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-08-12 17:34:41 +0900

    [Feat] verify invitation currentmember 적용 및 integration test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO member (member_id, nickname, social_id) VALUES
 ('f383cdb3-a871-4410-b146-fb1f7b447b9e', 'nickname', 'socialId');
+
+INSERT INTO room (room_id, title, capacity, start_date, end_date, invitation) VALUES
+(1,'제목',13, '2022-10-09','2022-10-20', 'XYQOTE');
--- src/test/java/com/firefighter/aenitto/rooms/integration/RoomIntegrationTest.java
+++ src/test/java/com/firefighter/aenitto/rooms/integration/RoomIntegrationTest.java
@@ -1,23 +1,24 @@
 package com.firefighter.aenitto.rooms.integration;
 
-import com.firefighter.aenitto.members.domain.Member;
-import com.firefighter.aenitto.members.dto.request.ChangeNicknameRequest;
+
+import com.firefighter.aenitto.rooms.domain.Room;
 import com.firefighter.aenitto.rooms.dto.RoomRequestDtoBuilder;
 import com.firefighter.aenitto.rooms.dto.request.CreateRoomRequest;
+import com.firefighter.aenitto.rooms.dto.request.VerifyInvitationRequest;
 import com.firefighter.aenitto.support.IntegrationTest;
 import com.firefighter.aenitto.support.security.WithMockCustomMember;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WithMockCustomMember
 public class RoomIntegrationTest extends IntegrationTest {
+    private Room room;
 
     @DisplayName("방 생성 -> 성공")
     @Test
@@ -32,6 +33,24 @@ public class RoomIntegrationTest extends IntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
+    }
+
+    @DisplayName("초대코드 검증 -> 성공")
+    @Test
+    @WithMockCustomMember
+    void verifyInvitation_success() throws Exception {
+        // given
+        VerifyInvitationRequest request = VerifyInvitationRequest.builder()
+                .invitationCode("XYQOTE").build();
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/invitations/verification")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.capacity", is(13)))
+                .andExpect(jsonPath("$.title", is("제목")))
+                .andExpect(jsonPath("$.participatingCount", is(0)));
     }
 
 }
