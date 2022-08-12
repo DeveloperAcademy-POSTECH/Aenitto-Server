b847c1ff7d92ad8bb9b08258820afa2976a41aa6
LeoBang <bluke8489@gmail.com>
2022-08-12 16:15:14 +0900

    [Test] RelationTest 마니또 관계 생성 (#43)

---
+++ src/test/java/com/firefighter/aenitto/rooms/domain/RelationTest.java
@@ -0,0 +1,67 @@
+package com.firefighter.aenitto.rooms.domain;
+
+import com.firefighter.aenitto.members.MemberFixture;
+import com.firefighter.aenitto.members.domain.Member;
+import com.firefighter.aenitto.rooms.RoomFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class RelationTest {
+    @Autowired private EntityManager em;
+
+    Room room1;
+    List<Member> members = new ArrayList<>();
+    List<MemberRoom> memberRooms = new ArrayList<>();
+
+    @BeforeEach
+    void initTest() {
+        room1 = RoomFixture.transientRoomFixture(1, 10, 10);
+        for (int i = 0; i < 7; i++) {
+            Member member = MemberFixture.transientMemberFixture(i);
+            MemberRoom memberRoom = RoomFixture.transientMemberRoomFixture(i);
+            memberRoom.setMemberRoom(member, room1);
+            members.add(member);
+            memberRooms.add(memberRoom);
+        }
+    }
+
+    @DisplayName("마니또 관계 생성 테스트 - 성공")
+    @Test
+    void startRoulette_success() {
+        // given
+        members.stream().forEach((member -> em.persist(member)));
+        em.persist(room1);
+
+        em.flush();
+        em.clear();
+
+        // given 2
+        Room findRoom = em.find(Room.class, room1.getId());
+        Relation.createRelations(findRoom.getMemberRooms(), findRoom);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Room findRoom2 = em.find(Room.class, room1.getId());
+
+        // then
+        assertThat(findRoom2.getRelations().size()).isEqualTo(7);
+    }
+}
