b35167e1323ce35d2737d2ab10f2877d797d6b89
LeoBang <bluke8489@gmail.com>
2022-08-13 19:35:10 +0900

    [Fix] CurrentMember 어노테이션 적용 (#43)

--- src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
+++ src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
@@ -37,54 +37,48 @@ public class RoomController {
 
     @PostMapping("/invitations/verification")
     public ResponseEntity verifyInvitation(
+            @CurrentMember Member member,
             @Valid @RequestBody final VerifyInvitationRequest request
     ) {
-        final Member member = mockLoginMember();
         final VerifyInvitationResponse response = roomService.verifyInvitation(member, request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/rooms/{roomId}/participants")
     public ResponseEntity participateRoom(
+            @CurrentMember Member member,
             @PathVariable final Long roomId,
             @RequestBody final ParticipateRoomRequest request
     ) {
-        final Member member = mockLoginMember();
         roomService.participateRoom(member, roomId, request);
         return ResponseEntity.created(URI.create("/api/v1/rooms/" + roomId)).build();
     }
 
     @GetMapping("/rooms/{roomId}/state")
     public ResponseEntity<GetRoomStateResponse> getRoomState(
+            @CurrentMember Member currentMember,
             @PathVariable final Long roomId
     ) {
-        final Member member = mockLoginMember();
-        return ResponseEntity.ok(roomService.getRoomState(member, roomId));
+        return ResponseEntity.ok(roomService.getRoomState(currentMember, roomId));
     }
 
     // TODO: RoomAPI 메타데이터 Response Header 에 넣기 (22.08.07)
     @GetMapping("/rooms")
     public ResponseEntity<ParticipatingRoomsResponse> findParticipatingRooms(
+            @CurrentMember Member member,
             @RequestParam(required = false) Long cursor,
             @RequestParam(defaultValue = "3") int limit
     ) {
-        final Member member = mockLoginMember();
         // cursor 있으면, next 가 있어야 함.
         return ResponseEntity.ok(roomService.getParticipatingRooms(member, cursor, limit));
     }
 
     @PatchMapping("/rooms/{roomId}/state")
     public ResponseEntity startAenitto(
+            @CurrentMember Member member,
             @PathVariable Long roomId
     ) {
-        final Member member = mockLoginMember();
         roomService.startAenitto(member, roomId);
         return ResponseEntity.noContent().build();
-    }
-
-    private Member mockLoginMember() {
-        return Member.builder()
-                .nickname("Mock")
-                .build();
     }
 }
