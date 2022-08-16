07c5f3c3488ab9d59e43093707c3a67b6044aae9
LeoBang <bluke8489@gmail.com>
2022-08-16 15:15:31 +0900

    [Feat] RoomService 방 상세정보 조회 (POST, PROCESSING) (#43) - CustomException 추가 - Dto 편의 메서드 추가

--- src/main/java/com/firefighter/aenitto/rooms/dto/response/RoomDetailResponse.java
+++ src/main/java/com/firefighter/aenitto/rooms/dto/response/RoomDetailResponse.java
@@ -1,6 +1,8 @@
 package com.firefighter.aenitto.rooms.dto.response;
 
+import com.firefighter.aenitto.missions.domain.Mission;
 import com.firefighter.aenitto.rooms.domain.MemberRoom;
+import com.firefighter.aenitto.rooms.domain.Relation;
 import com.firefighter.aenitto.rooms.domain.Room;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -47,32 +49,54 @@ public class RoomDetailResponse {
                 .build();
     }
 
-    public static RoomDetailResponse buildProcessingResponse(Room room, MemberRoom memberRoom) {
+    public static RoomDetailResponse buildProcessingResponse(Room room, Relation relation, MemberRoom memberRoom, boolean didView, Mission mission, int messageCount) {
         return RoomDetailResponse.builder()
                 .room(RoomDetail.of(room))
-                .didViewRoulette(memberRoom.didViewManitto())
+                .manittee(ManitteeInfo.of(relation))
+                .mission(MissionInfo.of(mission))
+                .didViewRoulette(didView)
+                .admin(memberRoom.isAdmin())
+                .messages(new MessageInfo(messageCount))
                 .build();
     }
 
-    public static RoomDetailResponse buildPostResponse(Room room, MemberRoom memberRoom) {
+    public static RoomDetailResponse buildPostResponse(Room room, Relation relation, MemberRoom memberRoom, int messageCount) {
         return RoomDetailResponse.builder()
                 .room(RoomDetail.of(room))
+                .admin(memberRoom.isAdmin())
+                .manittee(ManitteeInfo.of(relation))
+                .messages(new MessageInfo(messageCount))
                 .build();
     }
 
     @Getter
+    @Builder
     @AllArgsConstructor
     @NoArgsConstructor(force = true)
     public static class ManitteeInfo {
         private final String nickname;
+
+        public static ManitteeInfo of(Relation relation) {
+            return ManitteeInfo.builder()
+                    .nickname(relation.getManittee().getNickname())
+                    .build();
+        }
     }
 
     @Getter
+    @Builder
     @AllArgsConstructor
     @NoArgsConstructor(force = true)
     public static class MissionInfo {
         private final Long id;
         private final String content;
+
+        public static MissionInfo of(Mission mission) {
+            return MissionInfo.builder()
+                    .id(mission.getId())
+                    .content(mission.getContent())
+                    .build();
+        }
     }
 
     @Getter
