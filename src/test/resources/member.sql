3c13adf8f875fa84779f5b959e4afa83646573b3
LeoBang <bluke8489@gmail.com>
2022-08-17 03:24:16 +0900

    [Test] test용 .sql (#43)

---
+++ src/test/resources/member.sql
@@ -0,0 +1,15 @@
+-- Member2
+INSERT INTO member (member_id, nickname, social_id) VALUES
+('4ba90eab-c62b-4d44-aadb-b7f3183ea83e', 'nickname2', 'socialId2');
+
+-- Member3
+INSERT INTO member (member_id, nickname, social_id) VALUES
+('19452518-63ed-46fb-b0f0-19c0bdf97d7b', 'nickname3', 'socialId3');
+
+-- Member4
+INSERT INTO member (member_id, nickname, social_id) VALUES
+('34c45053-41df-44ef-8f00-e8f3f10b89a9', 'nickname4', 'socialId4');
+
+-- Member5
+INSERT INTO member (member_id, nickname, social_id) VALUES
+('e42085a8-08c5-45d5-8d58-ec39da152dbe', 'nickname5', 'socialId5');
---
+++ src/test/resources/relation.sql
@@ -0,0 +1,19 @@
+-- Relation1
+INSERT INTO relation (relation_id, manitto_id, manittee_id, room_id) VALUES
+(1, 'f383cdb3-a871-4410-b146-fb1f7b447b9e', '4ba90eab-c62b-4d44-aadb-b7f3183ea83e', 2);
+
+-- Relation2
+INSERT INTO relation (relation_id, manitto_id, manittee_id, room_id) VALUES
+(2, '4ba90eab-c62b-4d44-aadb-b7f3183ea83e', '19452518-63ed-46fb-b0f0-19c0bdf97d7b', 2);
+
+-- Relation3
+INSERT INTO relation (relation_id, manitto_id, manittee_id, room_id) VALUES
+(3, '19452518-63ed-46fb-b0f0-19c0bdf97d7b', '34c45053-41df-44ef-8f00-e8f3f10b89a9', 2);
+
+-- Relation4
+INSERT INTO relation (relation_id, manitto_id, manittee_id, room_id) VALUES
+(4, '34c45053-41df-44ef-8f00-e8f3f10b89a9', 'e42085a8-08c5-45d5-8d58-ec39da152dbe', 2);
+
+-- Relation5
+INSERT INTO relation (relation_id, manitto_id, manittee_id, room_id) VALUES
+(5, 'e42085a8-08c5-45d5-8d58-ec39da152dbe', 'f383cdb3-a871-4410-b146-fb1f7b447b9e', 2);
