da06a69bc3b1e888864f5d6497fd41e9593bad29
LeoBang <bluke8489@gmail.com>
2022-08-18 01:21:24 +0900

    [Chore] SqlPath 클래스 정의  (#64) - 원래 .sql 파일 path 빌더 클래스를 만드려했는데 annotation이 compile time 에서 constant한 상수만 받을 수 있어서 그냥 static String 셔틀로 만듦...

---
+++ src/main/java/com/firefighter/aenitto/common/utils/SqlPath.java
@@ -0,0 +1,14 @@
+package com.firefighter.aenitto.common.utils;
+
+
+public class SqlPath {
+    public static final String ROOM_GET_PARTICIPATING_ROOMS = "classpath:sql/api/room-getParticipatingRooms.sql";
+    public static final String MEMBER = "classpath:sql/member/member.sql";
+    public static final String MEMBER_ROOM = "classpath:sql/memberRoom/memberRoom.sql";
+    public static final String MESSAGE = "classpath:sql/message/message.sql";
+    public static final String MISSION = "classpath:sql/mission/mission.sql";
+    public static final String RELATION = "classpath:sql/relation/relation.sql";
+    public static final String ROOM_PRE = "classpath:sql/room/room-pre.sql";
+    public static final String ROOM_PROCESSING = "classpath:sql/room/room-processing.sql";
+    public static final String ROOM_POST = "classpath:sql/room/room-post.sql";
+}
