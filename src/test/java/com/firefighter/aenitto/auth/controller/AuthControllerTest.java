605d286da1f7d74c17eecef3b4542699c7898d90
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-11-15 17:09:44 +0900

    [Docs] 메세지 보내기 api 애러 메시지 문서화

--- src/test/java/com/firefighter/aenitto/auth/controller/AuthControllerTest.java
+++ src/test/java/com/firefighter/aenitto/auth/controller/AuthControllerTest.java
@@ -17,6 +17,7 @@ import com.firefighter.aenitto.auth.dto.response.LoginResponse;
 
 import com.firefighter.aenitto.common.exception.auth.FailedToFetchPublicKeyException;
 import com.firefighter.aenitto.common.exception.auth.InvalidIdentityTokenException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -63,173 +64,183 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class AuthControllerTest {
 
-    @InjectMocks
-    private AuthController target;
+	@InjectMocks
+	private AuthController target;
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    private ObjectMapper objectMapper;
+	private ObjectMapper objectMapper;
 
-    @Mock
-    @Qualifier("authServiceImpl")
-    private AuthService authService;
+	@Mock
+	@Qualifier("authServiceImpl")
+	private AuthService authService;
 
-    @Mock
-    @Qualifier("authServiceImplV2")
-    private AuthServiceV2 authServiceV2;
+	@Mock
+	@Qualifier("authServiceImplV2")
+	private AuthServiceV2 authServiceV2;
 
-    @BeforeEach
-    void init(RestDocumentationContextProvider restDocumentation) {
-        mockMvc = MockMvcBuilders.standaloneSetup(target)
-                .setControllerAdvice(GlobalExceptionHandler.class)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-        objectMapper = new ObjectMapper();
-    }
+	@BeforeEach
+	void init(RestDocumentationContextProvider restDocumentation) {
+		mockMvc = MockMvcBuilders.standaloneSetup(target)
+			.setControllerAdvice(GlobalExceptionHandler.class)
+			.apply(documentationConfiguration(restDocumentation))
+			.build();
+		objectMapper = new ObjectMapper();
+	}
 
-    @DisplayName("회원가입-로그인 / 실패 - identityToken 유효하지 않음")
-    @Test
-    void signUplogin_fail_Invalid_identityToken() throws Exception {
-        //given
-        final String uri = "/api/v1/login";
+	@DisplayName("회원가입-로그인 / 실패 - identityToken 유효하지 않음")
+	@Test
+	void signUplogin_fail_Invalid_identityToken() throws Exception {
+		//given
+		final String uri = "/api/v1/login";
 
-        doThrow(new InvalidIdentityTokenException())
-                .when(authService)
-                .loginOrSignIn(any());
+		doThrow(new InvalidIdentityTokenException())
+			.when(authService)
+			.loginOrSignIn(any());
 
-        //when, then
-        mockMvc.perform(
-                        RestDocumentationRequestBuilders.post(uri)
-                                .content(objectMapper.writeValueAsString(
-                                        LoginRequest.builder().identityToken("token이요").build()))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message", AuthErrorCode.INVALID_IDENTITY_TOKEN.getMessage()).exists())
-                .andExpect(jsonPath("$.status", AuthErrorCode.INVALID_IDENTITY_TOKEN.getStatus()).exists())
-                .andExpect(jsonPath("$.timestamp").exists())
-                .andExpect(jsonPath("$.errors").exists());
-    }
+		//when, then
+		mockMvc.perform(
+				RestDocumentationRequestBuilders.post(uri)
+					.content(objectMapper.writeValueAsString(
+						LoginRequest.builder().identityToken("token이요").build()))
+					.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message", AuthErrorCode.INVALID_IDENTITY_TOKEN.getMessage()).exists())
+			.andExpect(jsonPath("$.status", AuthErrorCode.INVALID_IDENTITY_TOKEN.getStatus()).exists())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.errors").exists())
+			.andDo(document("회원가입 및 로그인V1 - 실패 (유효하지 않은 identityToken)",
+				preprocessResponse(prettyPrint()),
+				responseFields(
+					fieldWithPath("message").description("ACCESS TOKEN"),
+					fieldWithPath("status").description("재발급 토큰"),
+					fieldWithPath("timestamp").description("ACCESS TOKEN"),
+					fieldWithPath("errors").description("ACCESS TOKEN")
 
+				)
+			));
+	}
 
-    @DisplayName("토큰 재발급 / 실패 - 유효한 토큰이 아님")
-    @Test
-    void reissueToken_fail_not_valid_token() throws Exception {
-        // given
-        final String url = "/api/v1/auth/reissue";
-        ReissueTokenRequest request = ReissueTokenRequest.builder()
-                .accessToken("accessToken").refreshToken("refreshToken").build();
-        doThrow(new InvalidTokenException()).when(authService)
-                .reissueAccessToken(any(ReissueTokenRequest.class));
+	@DisplayName("토큰 재발급 / 실패 - 유효한 토큰이 아님")
+	@Test
+	void reissueToken_fail_not_valid_token() throws Exception {
+		// given
+		final String url = "/api/v1/auth/reissue";
+		ReissueTokenRequest request = ReissueTokenRequest.builder()
+			.accessToken("accessToken").refreshToken("refreshToken").build();
+		doThrow(new InvalidTokenException()).when(authService)
+			.reissueAccessToken(any(ReissueTokenRequest.class));
 
-        // when
-        ResultActions perform = mockMvc.perform(
-                MockMvcRequestBuilders.patch(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON));
+		// when
+		ResultActions perform = mockMvc.perform(
+			MockMvcRequestBuilders.patch(url)
+				.content(objectMapper.writeValueAsString(request))
+				.contentType(MediaType.APPLICATION_JSON));
 
-        // then
-        perform
-                .andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.timestamp").exists())
-                .andExpect(jsonPath("$.status", is(AuthErrorCode.INVALID_TOKEN.getStatus().value())))
-                .andExpect(jsonPath("$.message", is(AuthErrorCode.INVALID_TOKEN.getMessage())));
-    }
+		// then
+		perform
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.status", is(AuthErrorCode.INVALID_TOKEN.getStatus().value())))
+			.andExpect(jsonPath("$.message", is(AuthErrorCode.INVALID_TOKEN.getMessage())));
+	}
 
-    @DisplayName("토큰 재발급 / 성공")
-    @Test
-    void reissueToken_success() throws Exception {
-        // given
-        final String url = "/api/v1/auth/reissue";
-        ReissueTokenRequest request = ReissueTokenRequest.builder()
-                .accessToken("accessToken").refreshToken("refreshToken").build();
-        doReturn(ReissueTokenResponse.builder()
-                .accessToken("accessToken").refreshToken("refreshToken").build())
-                .when(authService).reissueAccessToken(any(ReissueTokenRequest.class));
+	@DisplayName("토큰 재발급 / 성공")
+	@Test
+	void reissueToken_success() throws Exception {
+		// given
+		final String url = "/api/v1/auth/reissue";
+		ReissueTokenRequest request = ReissueTokenRequest.builder()
+			.accessToken("accessToken").refreshToken("refreshToken").build();
+		doReturn(ReissueTokenResponse.builder()
+			.accessToken("accessToken").refreshToken("refreshToken").build())
+			.when(authService).reissueAccessToken(any(ReissueTokenRequest.class));
 
-        // when
-        ResultActions perform = mockMvc.perform(
-                MockMvcRequestBuilders.patch(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON));
+		// when
+		ResultActions perform = mockMvc.perform(
+			MockMvcRequestBuilders.patch(url)
+				.content(objectMapper.writeValueAsString(request))
+				.contentType(MediaType.APPLICATION_JSON));
 
-        // then
-        perform
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document("토큰 재발급",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("accessToken").description("ACCESS TOKEN"),
-                                fieldWithPath("refreshToken").description("재발급 토큰")
-                        ),
-                        responseFields(
-                                fieldWithPath("accessToken").description("ACCESS TOKEN"),
-                                fieldWithPath("refreshToken").description("재발급 토큰")
-                        )
-                ));
-        ;
-    }
+		// then
+		perform
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andDo(document("토큰 재발급",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("accessToken").description("ACCESS TOKEN"),
+					fieldWithPath("refreshToken").description("재발급 토큰")
+				),
+				responseFields(
+					fieldWithPath("accessToken").description("ACCESS TOKEN"),
+					fieldWithPath("refreshToken").description("재발급 토큰")
+				)
+			));
+		;
+	}
 
-    @DisplayName("회원가입-로그인 / 실패 - apple public key 가져오기 실패")
-    @Test
-    void signUplogin_fail_apple_public_key() throws Exception {
-        //given
-        final String uri = "/api/v1/login";
+	@DisplayName("회원가입-로그인 / 실패 - apple public key 가져오기 실패")
+	@Test
+	void signUplogin_fail_apple_public_key() throws Exception {
+		//given
+		final String uri = "/api/v1/login";
 
-        doThrow(new FailedToFetchPublicKeyException())
-                .when(authService)
-                .loginOrSignIn(any());
+		doThrow(new FailedToFetchPublicKeyException())
+			.when(authService)
+			.loginOrSignIn(any());
 
-        //when, then
-        mockMvc.perform(
-                        RestDocumentationRequestBuilders.post(uri)
-                                .content(objectMapper.writeValueAsString(
-                                        LoginRequest.builder().identityToken("token이요").build()))
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.message", AuthErrorCode.APPLE_PUBLIC_KEY_FAILURE.getMessage()).exists())
-                .andExpect(jsonPath("$.status", AuthErrorCode.APPLE_PUBLIC_KEY_FAILURE.getStatus()).exists())
-                .andExpect(jsonPath("$.timestamp").exists())
-                .andExpect(jsonPath("$.errors").exists());
-    }
+		//when, then
+		mockMvc.perform(
+				RestDocumentationRequestBuilders.post(uri)
+					.content(objectMapper.writeValueAsString(
+						LoginRequest.builder().identityToken("token이요").build()))
+					.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isInternalServerError())
+			.andExpect(jsonPath("$.message", AuthErrorCode.APPLE_PUBLIC_KEY_FAILURE.getMessage()).exists())
+			.andExpect(jsonPath("$.status", AuthErrorCode.APPLE_PUBLIC_KEY_FAILURE.getStatus()).exists())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.errors").exists())
+		;
+	}
 
-    @DisplayName("회원가입-로그인 / 성공")
-    @Test
-    void signUplogin_success() throws Exception {
-        //given
-        final String uri = "/api/v1/login";
+	@DisplayName("회원가입-로그인 / 성공")
+	@Test
+	void signUplogin_success() throws Exception {
+		//given
+		final String uri = "/api/v1/login";
 
-        LoginRequest request = LoginRequest.builder().identityToken("token이요").build();
-        LoginResponse response = LoginResponse.builder().accessToken("accessToken").refreshToken("refreshToken")
-                .isNewMember(true).userSettingDone(true).build();
-        when(authService.loginOrSignIn(any())).thenReturn(response);
+		LoginRequest request = LoginRequest.builder().identityToken("token이요").build();
+		LoginResponse response = LoginResponse.builder().accessToken("accessToken").refreshToken("refreshToken")
+			.isNewMember(true).userSettingDone(true).build();
+		when(authService.loginOrSignIn(any())).thenReturn(response);
 
-        //when, then, docs
-        mockMvc.perform(
-                        RestDocumentationRequestBuilders.post(uri)
-                                .content(objectMapper.writeValueAsString(request))
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.accessToken").exists())
-                .andDo(document("회원가입 및 로그인V1",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("identityToken").description("애플의 identityToken")
-                        ),
-                        responseFields(
-                                fieldWithPath("accessToken").description("토큰"),
-                                fieldWithPath("nickname").description("닉네임"),
-                                fieldWithPath("refreshToken").description("재발급 토큰"),
-                                fieldWithPath("isNewMember").description("새로운 멤버인지 기존 멤버인지"),
-                                fieldWithPath("userSettingDone").description("닉네임 정보 입력했는지 여부")
-                        )
-                ));
-    }
+		//when, then, docs
+		mockMvc.perform(
+				RestDocumentationRequestBuilders.post(uri)
+					.content(objectMapper.writeValueAsString(request))
+					.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.accessToken").exists())
+			.andDo(document("회원가입 및 로그인V1",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("identityToken").description("애플의 identityToken")
+				),
+				responseFields(
+					fieldWithPath("accessToken").description("토큰"),
+					fieldWithPath("nickname").description("닉네임"),
+					fieldWithPath("refreshToken").description("재발급 토큰"),
+					fieldWithPath("isNewMember").description("새로운 멤버인지 기존 멤버인지"),
+					fieldWithPath("userSettingDone").description("닉네임 정보 입력했는지 여부")
+				)
+			));
+	}
 }
