4a39e5336c269046bdbbe3815dd50bf177b31d81
LeoBang <bluke8489@gmail.com>
2022-12-27 02:32:40 +0900

    [Refactor] IntegrationTest 설정 수정 (#154) - ImageFixture 의 MockMultipartFile mock의 이름을 API controller가 잡아챌 수 있는 이름인 "image"로 수정 - IntegrationTest 에 AmazonS3을 위한 서비스 객체 stub 작성   - update(), upload() 메서드

--- src/test/java/com/firefighter/aenitto/message/ImageFixture.java
+++ src/test/java/com/firefighter/aenitto/message/ImageFixture.java
@@ -4,7 +4,7 @@ import org.apache.http.entity.ContentType;
 import org.springframework.mock.web.MockMultipartFile;
 
 public class ImageFixture {
-    public static MockMultipartFile IMAGE = new MockMultipartFile("files",
+    public static MockMultipartFile IMAGE = new MockMultipartFile("image",
             "test.jpg",
             ContentType.IMAGE_JPEG.getMimeType(),
             "테스트파일".getBytes());
--- src/test/java/com/firefighter/aenitto/support/IntegrationTest.java
+++ src/test/java/com/firefighter/aenitto/support/IntegrationTest.java
@@ -1,11 +1,20 @@
 package com.firefighter.aenitto.support;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,11 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import com.firefighter.aenitto.messages.service.StorageS3ServiceImpl;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,9 +39,14 @@ public class IntegrationTest {
     protected EntityManager em;
     @Autowired
     protected ObjectMapper objectMapper;
+    @MockBean
+    protected StorageS3ServiceImpl storageS3Service;
     @Autowired
     WebApplicationContext webApplicationContext;
     protected MockMvc mockMvc;
+
+    protected static final UUID MOCK_USER_ID = UUID.fromString("f383cdb3-a871-4410-b146-fb1f7b447b9e");
+    protected static final String STORAGE_SAVED_IMG_URL = "sampleUrl";
 
     @BeforeEach
     void setUp() {
@@ -41,6 +55,14 @@ public class IntegrationTest {
                 .alwaysDo(print())
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .build();
+    }
+
+    @BeforeEach
+    void stubS3StorageService() {
+        when(storageS3Service.getUrl(anyString()))
+            .thenReturn(STORAGE_SAVED_IMG_URL);
+        doNothing()
+            .when(storageS3Service).upload(anyString(), any(InputStream.class), any(ObjectMetadata.class));
     }
 
     protected void flushAndClear() {
