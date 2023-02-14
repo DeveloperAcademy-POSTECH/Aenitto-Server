54a4dbd547b11f89f0d9d57544bd8f1a010971e5
jola7373 <62223855+jola7373@users.noreply.github.com>
2023-02-14 20:55:53 +0900

    [Feat] message에 missionId 추가

--- src/main/java/com/firefighter/aenitto/messages/domain/Message.java
+++ src/main/java/com/firefighter/aenitto/messages/domain/Message.java
@@ -46,6 +46,9 @@ public class Message extends CreationLog {
 	@Column
 	private String imgUrl;
 
+	@Column
+	private Long missionId;
+
 	public void sendMessage(Member sender, Member receiver, Room room) {
 		this.sender = sender;
 		this.receiver = receiver;
@@ -59,12 +62,13 @@ public class Message extends CreationLog {
 	}
 
 	@Builder
-	public Message(String content, String imgUrl) {
+	public Message(String content, String imgUrl, Long missionId) {
 		this.content = content;
 		this.imgUrl = imgUrl;
+		this.missionId = missionId;
 	}
 
-	public static Message initializeMessageRelationship(String content, Relation relation) {
+	public static Message initializeMessageRelationship(String content, Relation relation, Long missionId) {
 		Message message = Message.builder().content(content).build();
 		message.setMessageRelationship(relation);
 		return message;
