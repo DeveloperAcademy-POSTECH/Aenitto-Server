5829432aa61262b0e9580acf9b67f0726d28271b
ckyeon <qsky8520@gmail.com>
2023-06-27 04:24:25 +0900

    fix(MissionRepository): using native query

--- src/main/java/com/firefighter/aenitto/missions/repository/MissionRepository.java
+++ src/main/java/com/firefighter/aenitto/missions/repository/MissionRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
-  @Query(value = "SELECT m FROM Mission m WHERE m.type = :missionType ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+  @Query(value = "SELECT * FROM mission WHERE type = :missionType ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
   Optional<Mission> findRandomMission(MissionType missionType);
 }
