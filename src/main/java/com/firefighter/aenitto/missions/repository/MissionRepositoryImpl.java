d565d0e58646209134a617bfdbeb4e82bba67482
ckyeon <qsky8520@gmail.com>
2023-06-27 04:46:39 +0900

    feat(MissionRepository): add custom repository caused by using entity manager

---
+++ src/main/java/com/firefighter/aenitto/missions/repository/MissionRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.firefighter.aenitto.missions.repository;
+
+import com.firefighter.aenitto.missions.domain.Mission;
+import com.firefighter.aenitto.missions.domain.MissionType;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MissionRepositoryImpl implements MissionRepositoryCustom {
+
+  private final EntityManager em;
+
+  @Override
+  public Optional<Mission> findRandomMission(MissionType missionType) {
+    return em.createQuery(
+            "SELECT m" +
+                " FROM Mission m" +
+                " WHERE m.type = :missionType" +
+                " ORDER BY random()", Mission.class)
+        .setParameter("missionType", missionType)
+        .getResultStream()
+        .findFirst();
+  }
+}
